8004d5a21df614a573439873f8351483d53fb91a
KamaleshS2 <158026936+KamaleshS2@users.noreply.github.com>
2024-02-12 07:35:25 +0000

    Commit

---
+++ src/test/java/runner/add.java
@@ -0,0 +1,10 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features= "src\\test\\resources\\Feature\\addition.feature",
+glue = "stepDefinitions")
+public class add extends AbstractTestNGCucumberTests{
+
+}
